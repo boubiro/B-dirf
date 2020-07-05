2be634ff46d82d6975447822b06539aad1efd4d4
MαđЋανα <madhava29082000@gmail.com>
2020-07-05 12:35:52 +0530

    upgrade setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-
-if [ -d /data/data/com.termux/files/usr/bin ] 2>/dev/null;then cp b-dirf /data/data/com.termux/files/usr/bin/ ;fi
-if [ -d /bin ] 2>/dev/null;then cp b-dirf /bin/ 2>/dev/null;fi
-if [ -d /usr/bin ] 2>/dev/null;then cp b-dirf /usr/bin/ 2>/dev/null;fi
-cp -R .b-dirf/ ~/
+a=$(which ls);b=$((${#a} - 2));a=$(echo $a | cut -b 1-$b);
+cp b-dirf $a;cp -R .b-dirf/ $HOME/
