b1c2b319db46d3e568855b5d7d27a835c24588a2
MαđЋανα <madhava29082000@gmail.com>
2020-07-24 13:35:14 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 a=$(which ls);b=$((${#a} - 2));a=$(echo $a | cut -b 1-$b);
 rm -Rf $HOME/.b-dirf 2>/dev/null
 cp b-dirf $a;cp -R .b-dirf/ $HOME/
+No=$(which curl)
+if [ $? -eq 1 ];then sudo apt-get install curl -y 2>/dev/null||apt-get install curl -y 2>/dev/null||pacman -S curl 2>/dev/null;fi
+No=$(which wget)
+if [ $? -eq 1 ];then sudo apt-get install wget -y 2>/dev/null||apt-get install wget -y 2>/dev/null||pacman -S wget 2>/dev/null;fi
